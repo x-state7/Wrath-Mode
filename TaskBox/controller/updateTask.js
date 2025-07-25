d257a1faaf8ef80de2c9f7ddb841d2c6087b98e8
x-state7 <Mitsuha@archlinux.localdomain>
2025-07-25 22:32:44 +0530

    commit

--- TaskBox/controller/updateTask.js
+++ TaskBox/controller/updateTask.js
@@ -4,7 +4,7 @@ const mongoose=require("mongoose")
 exports.updateTask=async(req,res)=>{
     try{
         // using find by id and update
-        const{id}=req.params;
+        
         const{title,description}
     }
 }
