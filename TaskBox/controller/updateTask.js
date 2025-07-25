da349cee86f5650ee95a8037ce2e933436b39c78
x-state7 <Mitsuha@archlinux.localdomain>
2025-07-25 22:35:21 +0530

    writing more code

--- TaskBox/controller/updateTask.js
+++ TaskBox/controller/updateTask.js
@@ -2,9 +2,31 @@ const User=require("../models/User")
 const mongoose=require("mongoose")
 
 exports.updateTask=async(req,res)=>{
-    try{
-        // using find by id and update
-        
-        const{title,description}
-    }
+  try {
+    // using find by id and update from mooongoose
+    const { id } = req.params;
+    const { title, description } = req.body;
+    const todo = await Todo.findByIdAndUpdate(
+      { _id: id },
+      { title, description, updatedAt: Date.now() },
+    )
+
+    res.status(200)
+      .json({
+        success: true,
+        data: todo,
+        message: "updated successfully",
+      })
+
+  }
+  catch (error) {
+    console.error(error);
+    res.status(500)
+      .json({
+        success: false,
+        data: "internal server error",
+        message: error.message,
+      })
+
+  }
 }
